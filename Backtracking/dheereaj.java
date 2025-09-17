ffe574cbc2ec21a4f98ef7381ec84923975bba77
Dheeraj Kumar <dhiruk671@gmail.com>
2025-09-17 21:40:51 +0530

    add backtracking array implementation and initial structure for dheereaj class

---
+++ Backtracking/dheereaj.java
@@ -0,0 +1,13 @@
+
+
+public class dheereaj {
+    public static int todheeraj(int n) {
+        if (n<arr.length) {
+            
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+    
+}
